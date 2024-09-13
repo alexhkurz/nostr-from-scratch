7877f012df1d75739a4713d64792f5be773f1ba7
Alexander Kurz <alexhkurz@gmail.com>
2024-09-13 12:07:28 -0700

    refactor: Remove pkg_resources dependency and simplify module check in nostr_example.py

--- nostr_example.py
+++ nostr_example.py
@@ -1,5 +1,4 @@
 import sys
-import pkg_resources
 import os
 
 # Debugging: List contents of the nostr package directory
@@ -15,14 +14,6 @@ print(sys.path)
 print("Python executable:", sys.executable)
 
 try:
-    pkg_resources.get_distribution('nostr')
-    print("'nostr' module is installed.")
-except pkg_resources.DistributionNotFound:
-    print("'nostr' module is not installed.")
-    print("Please install it using 'pip install nostr'.")
-    exit(1)
-
-try:
     from nostr.relay import Relay
     from nostr.event import Event
     from nostr.key import PrivateKey
@@ -35,7 +26,7 @@ except ModuleNotFoundError as e:
 private_key = PrivateKey()
 
 # Create a new event
-event = Event(
+event = Event(  
     public_key=private_key.public_key.hex(),
     content="Hello, Nostr!",
     kind=1
